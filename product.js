e89a11ffe29ef028776f4971a63f589357d01c48
sanjaayanand <46858163+sanjaayanand@users.noreply.github.com>
2020-12-03 20:32:10 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,26 @@
+angular.module('myapp',[])
+.controller('myctrl',function($scope)
+{
+    $scope.listProducts=[
+        {id:'01',name:'sam',price:MCA},
+        {id:'02',name:'ram',price:MCA},
+        {id:'03',name:'som',price:MCA},
+        {id:'04',name:'deena',price:MSC}
+    ];
+    function getindex(id)
+    {
+        for(var i=0;i<$scope.listProducts.length;i++)
+        if($scope.listProducts[i].id==id)
+        return i;
+        return -1;
+    }
+    $scope.delete=function()
+    {
+        var result=confirm('once deleted they cannot be added again.');
+        if(result==true)
+        {
+            var index=this.$index;
+            $scope.listProducts.splice(index,1);
+        }
+    };
+});
