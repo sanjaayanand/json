5f39eb98207f60d2af69343472c4963d389ea2ba
sanjaayanand <46858163+sanjaayanand@users.noreply.github.com>
2020-12-04 08:52:36 +0530

    Update product.js

--- product.js
+++ product.js
@@ -14,6 +14,15 @@ angular.module('myapp',[])
         return i;
         return -1;
     }
+    $scope.edit=function(id)
+    {
+        var index=this.$index;
+        var product=$scope.listProducts[index];
+        $scope.id=product.id;
+        $scope.name=product.name;
+        $scope.price=product.price;
+        $scope.quantity=product.quantity;
+    }
     $scope.delete=function()
     {
         var result=confirm('once deleted they cannot be added again.');
